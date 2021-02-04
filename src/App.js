72b146c3fd1d70956c9c783a693408f674653ca2
joao-chass <joaoborges07@hotmail.com>
2021-02-03 21:01:00 -0300

    abc

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and osioisokjdskljdf
+          Edit <code>src/App.js</code> and joão pedro é feio
         </p>
         <a
           className="App-link"
