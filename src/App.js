811faba09a9b1d8c9e371282c28b3a46e0166771
João Pedro <61188401+DemoZero@users.noreply.github.com>
2021-02-03 21:06:51 -0300

    teste do joao pedro

--- src/App.js
+++ src/App.js
@@ -1,26 +1,10 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-<<<<<<< HEAD
-          Edit <code>src/App.js</code> and joão pedro é feio
-=======
-          Edit <code>src/App.js</code> and osioisokjdskljdf doido
->>>>>>> 32523f9e3de9e5620ebee819a23f995fc46970f8
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        
       </header>
     </div>
   );
