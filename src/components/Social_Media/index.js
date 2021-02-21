d0ae928d1783258c335dd917ab0dc0df68b0d070
p9103 <79404888+p9103@users.noreply.github.com>
2021-02-21 11:46:12 -0300

    Update index.js

--- src/components/Social_Media/index.js
+++ src/components/Social_Media/index.js
@@ -1,10 +1,5 @@
 import styled from 'styled-components';
-import fb from './imagens/FB.png';
-import insta from './imagens/IG.png';
-import wpp from './imagens/WPP.png';
-import Linkedin from './imagens/LinkedIn.png';
-import Twitter from './imagens/Twitter.png';
-import YouTube from './imagens/YouTube.png'
+import { fb, insta, wpp, Linkedin, Twitter, YouTube } from './Images'
 
 const Social = styled.div`
     display: flex;
