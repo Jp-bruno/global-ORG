f7c88854816f844190f197b963ef59ec08e15f28
Jp-bruno <77502084+Jp-bruno@users.noreply.github.com>
2021-03-11 21:00:08 -0300

    Add files via upload

--- src/index.js
+++ src/index.js
@@ -1,23 +1,17 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import "./index.css";
-import App from "./pages/Home";
-import { BrowserRouter, Switch, Route } from "react-router-dom";
-import CadastroONG from "./pages/Cadastro/ONG";
-import CadastroVaga from "./pages/Cadastro/Vaga";
-import CadastroVoluntario from "./pages/Cadastro/Voluntario";
-
-const Pagina404 = () => <div>Página não encontrada 404</div>;
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import Home from './Home';
+import styled from 'styled-components';
 
 ReactDOM.render(
-  <BrowserRouter>
-    <Switch>
-      <Route path="/" component={App} exact />
-      <Route path="/Cadastro/ONG" component={CadastroONG} />
-      <Route path="/Cadastro/Voluntario" component={CadastroVoluntario} />
-      <Route path="/Cadastro/Vaga" component={CadastroVaga} />
-      <Route component={Pagina404} />
-    </Switch>
-  </BrowserRouter>,
-  document.getElementById("root")
+  <React.StrictMode>
+    <BrowserRouter>
+      <Switch>
+        <Route path="/" component={Home} exact />
+      </Switch>
+    </BrowserRouter>
+  </React.StrictMode>,
+  document.getElementById('root')
 );
+
